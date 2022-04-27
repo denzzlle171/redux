560a6b03b5c26632875584252fa09c3a7b3751c1
Denys <denzzlle171@gmail.com>
2022-04-28 00:35:38 +0300

    test 3

--- lesson6/task2/src/users/UserInfo.jsx
+++ lesson6/task2/src/users/UserInfo.jsx
@@ -23,7 +23,7 @@ if (isFetching) {
       />
       <div className="user__info">
         <span className="user__name">{userData.name}</span>
-        <span className="user__location">{`from ${userData.location}`}</span>
+        <span className="user__location">{userData.location}</span>
       </div>
     </div>
   );
