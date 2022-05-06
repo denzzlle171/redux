a6f9b73e074285d3795ef95d6b5bf0eca029e317
Denys <denzzlle171@gmail.com>
2022-05-06 14:09:44 +0300

    save

--- lesson7/task1/src/store.js
+++ lesson7/task1/src/store.js
@@ -7,28 +7,11 @@ const reducer = combineReducers({
   tasks: tasksReducer,
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  || compose;
 
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store
-
-
-
-
-
-
-
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk'
-// import weatherReducer from './weather/weather.reducer';
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(
-//   weatherReducer,
-//   composeEnhancers(applyMiddleware(thunk))
-// )
-// export default store;
-
+ 
